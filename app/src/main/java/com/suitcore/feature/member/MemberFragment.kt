859c939c6fdfa602b095af341c53b81a5c3d2ed6
Dody Darmawan <dody.darmawandpk@gmail.com>
2021-10-13 12:56:38 +0700

    prevent force dark mode in first app launch

--- app/src/main/java/com/suitcore/feature/member/MemberFragment.kt
+++ app/src/main/java/com/suitcore/feature/member/MemberFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.suitcore.R
 import com.suitcore.base.ui.BaseFragment
 import com.suitcore.base.ui.recyclerview.BaseRecyclerView
